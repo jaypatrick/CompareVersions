4ad2a0ff000341e3a3bd4ccfba163f029ef8e4af
Jayson <jayson.knight@jaysonknight.com>
2023-01-18 19:59:02 -0500

    Added custom attribute
    Added custom attribute

---
+++ src/CompareVersions/Attributes/VersionableAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace CompareVersions.Attributes
+{
+    [AttributeUsage((AttributeTargets.Class))]
+    internal class VersionableAttribute<T> : Attribute
+    where T : IVersionable
+
+    {
+        /// <summary>
+        /// Gets the type of the parameter.
+        /// </summary>
+        /// <value>
+        /// The type of the parameter.
+        /// </value>
+        public T ParamType { get; }
+    }
+
+    internal interface IVersionable
+    {
+    }
+}
